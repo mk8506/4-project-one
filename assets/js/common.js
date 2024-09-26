c519f5fabc98148af08d85ff8030aba513c92275
rlaalswl <siennalynnzyamin@gmail.com>
2024-09-26 17:47:49 +0900

    header dropdown

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,16 +2,28 @@
 //   document.querySelector("#boys").setAttribute("href", );
 // })();
 
-let lastScroll = document.documentElement.scrollTop || 0
-document.addEventListener('scroll', function(){
-  let scrollTop = document.documentElement.scrollTop
-  const scrollup = document.querySelector("#scrollup");
-  if(scrollTop > lastScroll) {
-    //down
-    scrollup.classList.remove("scrollup");
+// let lastScroll = document.documentElement.scrollTop || 0
+// document.addEventListener('scroll', function(){
+//   let scrollTop = document.documentElement.scrollTop
+//   const scrollup = document.querySelector("#scrollup");
+//   if(scrollTop > lastScroll) {
+//     //down
+//     scrollup.classList.remove("scrollup");
+//   } else {
+//     //up
+//     scrollup.classList.add("scrollup");
+//   }
+//   lastScroll = scrollTop;
+// })
+
+//Sign in dropdown
+const button = document.querySelector("#dd-btn");
+button.addEventListener('click', (e) => {
+  const sub = v.querySelectorAll("#dd");
+  console.log(sub.style.maxHeight);
+  if (sub.style.maxHeight === '0px' || sub.style.maxHeight === "") {
+    sub.style.maxHeight = sub.scrollHeight + 'px';
   } else {
-    //up
-    scrollup.classList.add("scrollup");
+    sub.style.maxHeight = '0px';
   }
-  lastScroll = scrollTop;
-})
+});
